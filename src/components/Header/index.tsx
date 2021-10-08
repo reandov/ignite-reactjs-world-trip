90ab960849a5681e130bbcd7ce399e8fad28cee5
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-10-07 23:29:32 -0300

    finished the structure of the application, optimization needed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,15 @@
 import { Flex, Image } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const router = useRouter();
+
   return (
     <Flex maxW="1680" m="auto" display="flex" align="center" p="6">
-      {/* <Image src="/assets/back.svg" position="absolute" /> */}
+      {router.query.slug && (
+        <Image src="/assets/back.svg" position="absolute" />
+      )}
+
       <Image src="/assets/logo.svg" margin="auto" />
     </Flex>
   );
--- src/pages/continents/[slug].tsx
+++ src/pages/continents/[slug].tsx
@@ -7,7 +7,10 @@ import {
   useBreakpointValue,
   Heading,
   Text,
+  Avatar,
+  Tooltip,
 } from "@chakra-ui/react";
+import QuestionOutlineIcon from "@chakra-ui/icon";
 import { useRouter } from "next/router";
 import db from "../../services/db.json";
 
@@ -20,6 +23,12 @@ interface Continent {
   amount_languages: number;
   amount_cities: number;
   image_path: string;
+  countries: {
+    city: string;
+    country: string;
+    city_img_path: string;
+    flag_path: string;
+  }[];
 }
 
 export default function Continent() {
@@ -60,37 +69,122 @@ export default function Continent() {
         </Heading>
       </Flex>
 
-      <HStack spacing="14" maxW="1280" my="20" mx="auto">
-        <Text flex="0.5" textAlign="justify" fontSize="2xl" color="gray.700">
+      <Flex
+        direction={isWideVersion ? "row" : "column"}
+        spacing="14"
+        maxW={["350", "1280"]}
+        my={["10", "20"]}
+        mx="auto"
+      >
+        <Text
+          flex="0.5"
+          textAlign="justify"
+          fontSize={["md", "2xl"]}
+          color="gray.700"
+        >
           {continent.about}
         </Text>
-        <HStack flex="0.5" spacing="24" justify="center" align="center">
+        <HStack
+          flex="0.5"
+          mt={["10", "0"]}
+          spacing={["8", "24"]}
+          justify="center"
+          align="center"
+        >
           <VStack align="center">
-            <Text fontSize="6xl" fontWeight="medium" color="orange.300">
+            <Text
+              fontSize={["5xl", "6xl"]}
+              fontWeight="medium"
+              color="orange.200"
+            >
               {continent.amount_countries}
             </Text>
-            <Text fontSize="xl" fontWeight="medium" color="gray.600">
+            <Text fontSize={["md", "xl"]} fontWeight="medium" color="gray.600">
               países
             </Text>
           </VStack>
           <VStack align="center">
-            <Text fontSize="6xl" fontWeight="medium" color="orange.300">
+            <Text
+              fontSize={["5xl", "6xl"]}
+              fontWeight="medium"
+              color="orange.200"
+            >
               {continent.amount_languages}
             </Text>
-            <Text fontSize="xl" fontWeight="medium" color="gray.600">
+            <Text fontSize={["md", "xl"]} fontWeight="medium" color="gray.600">
               línguas
             </Text>
           </VStack>
           <VStack align="center">
-            <Text fontSize="6xl" fontWeight="medium" color="orange.300">
+            <Text
+              fontSize={["5xl", "6xl"]}
+              fontWeight="medium"
+              color="orange.200"
+            >
               100
             </Text>
-            <Text fontSize="xl" fontWeight="medium" color="gray.600">
-              cidades +{continent.amount_cities - 100}
-            </Text>
+            <HStack>
+              <Text
+                fontSize={["md", "xl"]}
+                fontWeight="medium"
+                color="gray.600"
+              >
+                cidades +{continent.amount_cities - 100}
+              </Text>
+              <Tooltip
+                hasArrow
+                label="Este continente possui diversas cidades incríveis!"
+                bg="gray.300"
+                color="black"
+              >
+                <QuestionOutlineIcon />
+              </Tooltip>
+            </HStack>
           </VStack>
         </HStack>
-      </HStack>
+      </Flex>
+
+      <Box maxW={["350", "1280"]} mt="10" mx="auto">
+        <Text fontSize="4xl" fontWeight="medium" color="gray.700">
+          Cidades +{continent.amount_cities - 100}
+        </Text>
+      </Box>
+
+      <Flex
+        maxW={["350", "1280"]}
+        justify={isWideVersion ? "" : "center"}
+        flexWrap="wrap"
+        mx="auto"
+        my="10"
+        gridGap="12"
+      >
+        {continent.countries?.map((country, index) => (
+          <Box>
+            <Box
+              borderTopRadius="4"
+              bgImage={`url(${country.city_img_path})`}
+              bgPosition="center"
+              bgRepeat="no-repeat"
+              bgSize="cover"
+              height="3xs"
+              width="3xs"
+            />
+            <Flex
+              justify="space-between"
+              align="center"
+              p="2"
+              borderWidth="thin"
+              borderColor="orange.200"
+            >
+              <VStack align="flex-start">
+                <Text>{country.city}</Text>
+                <Text>{country.country}</Text>
+              </VStack>
+              <Avatar src={country.flag_path} size="sm" />
+            </Flex>
+          </Box>
+        ))}
+      </Flex>
     </Box>
   );
 }
